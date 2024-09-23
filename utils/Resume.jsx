980698f3625ec8f362bf35c940631f7e38acf52a
Tanishq1007 <132736717+Tanishq1007@users.noreply.github.com>
2024-09-23 11:22:51 +0530

    Update Resume

--- utils/Resume.jsx
+++ utils/Resume.jsx
@@ -5,7 +5,7 @@ const Resume = () => {
     <Fragment>
       <div className="dark:bg-[#0ab0c2] m-auto mt-6 py-2 px-6 rounded w-fit dark:hover:bg-[#078795] bg-[#f91071] hover:bg-[#c72c6c] text-white font-bold z-10">
         <a
-          href="https://drive.google.com/file/d/13K84VBayOeKsD8SpatBVQAuc-AJMQz2H/view?usp=drive_link"
+          href="https://drive.google.com/file/d/1daZqXslvc3D5-zgJ61SVI2EVqJlV7nm9/view?usp=drive_link"
           target="_blank"
         >
           Resume
